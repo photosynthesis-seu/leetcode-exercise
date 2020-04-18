b74752d1e5d3de4fa0ef0e32a8fdc27f1323a2f0
刘启汉 <41352825+photosynthesis-seu@users.noreply.github.com>
2020-04-18 22:50:43 +0800

    Update No.53-最大子序和.cpp

--- 题目和解答/动态规划/No.53-最大子序和.cpp
+++ 题目和解答/动态规划/No.53-最大子序和.cpp
@@ -18,3 +18,53 @@ public:
         return result;
     }
 };
+//分治算法
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值
+        int result = INT_MIN;
+        int numsSize = int(nums.size());
+        result = maxSubArrayHelper(nums, 0, numsSize - 1);
+        return result;
+    }
+
+    int maxSubArrayHelper(vector<int> &nums, int left, int right)
+    {
+        if (left == right)
+        {
+            return nums[left];
+        }
+        int mid = (left + right) / 2;
+        int leftSum = maxSubArrayHelper(nums, left, mid);
+        //注意这里应是mid + 1，否则left + 1 = right时，会无限循环
+        int rightSum = maxSubArrayHelper(nums, mid + 1, right);
+        int midSum = findMaxCrossingSubarray(nums, left, mid, right);
+        int result = max(leftSum, rightSum);
+        result = max(result, midSum);
+        return result;
+    }
+
+    int findMaxCrossingSubarray(vector<int> &nums, int left, int mid, int right)
+    {
+        int leftSum = INT_MIN;
+        int sum = 0;
+        for (int i = mid; i >= left; i--)
+        {
+            sum += nums[i];
+            leftSum = max(leftSum, sum);
+        }
+
+        int rightSum = INT_MIN;
+        sum = 0;
+        //注意这里i = mid + 1，避免重复用到nums[i]
+        for (int i = mid + 1; i <= right; i++)
+        {
+            sum += nums[i];
+            rightSum = max(rightSum, sum);
+        }
+        return (leftSum + rightSum);
+    }
+};
