da7f2b0a7b9701df6494c37159241118620c4eba
刘启汉 <41352825+photosynthesis-seu@users.noreply.github.com>
2020-04-25 15:47:49 +0800

    Create No.75-颜色分类.cpp

---
+++ 题目和解答/排序算法/No.75-颜色分类.cpp
@@ -0,0 +1,17 @@
+//荷兰旗问题
+void sortColors(vector<int>& nums) {
+    // 对于所有 idx < p0 : nums[idx < p0] = 0
+    // curr 是当前考虑元素的下标
+    int p0 = 0, curr = 0;
+    // 对于所有 idx > p2 : nums[idx > p2] = 2
+    int p2 = nums.size() - 1;
+    while (curr <= p2) {
+      if (nums[curr] == 0) {
+        swap(nums[curr++], nums[p0++]);
+      }
+      else if (nums[curr] == 2) {
+        swap(nums[curr], nums[p2--]);
+      }
+      else curr++;
+    }
+  }
